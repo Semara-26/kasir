eda4573cbe5337628abcccf0126f094a97c67954
Agung Deva <agungdeva985@gmail.com>
2025-06-13 14:14:10 +0800

    update crud barang

--- barang_tambah.php
+++ barang_tambah.php
@@ -8,11 +8,19 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
   $beli = $_POST['harga_beli'];
   $jual = $_POST['harga_jual'];
   $satuan = $_POST['satuan_barang'];
+  $stok = $_POST['stok'];
+  $id_toko = 1; // ganti sesuai implementasi multi toko
 
-  // Validasi sederhana
   if ($id_kategori != '') {
     mysqli_query($conn, "INSERT INTO barang (id_kategori, nama_barang, harga_beli, harga_jual, satuan_barang) 
                          VALUES ('$id_kategori', '$nama', '$beli', '$jual', '$satuan')");
+    
+    $id_barang_baru = mysqli_insert_id($conn);
+
+    // Tambahkan stok awal ke tabel stoktoko
+    mysqli_query($conn, "INSERT INTO stoktoko (id_toko, id_barang, jumlah_stok) 
+                         VALUES ('$id_toko', '$id_barang_baru', '$stok')");
+
     header('Location: barang.php');
     exit;
   } else {
@@ -39,12 +47,8 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         <option value="">-- Pilih Kategori --</option>
         <?php
         $kategori = mysqli_query($conn, "SELECT * FROM kategori");
-        if (!$kategori) {
-          echo "<option value=''>Gagal mengambil data kategori</option>";
-        } else {
-          while ($row = mysqli_fetch_assoc($kategori)) {
-            echo "<option value='" . $row['id_kategori'] . "'>" . $row['nama_kategori'] . "</option>";
-          }
+        while ($row = mysqli_fetch_assoc($kategori)) {
+          echo "<option value='" . $row['id_kategori'] . "'>" . $row['nama_kategori'] . "</option>";
         }
         ?>
       </select>
@@ -54,13 +58,14 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
       <label class="form-label">Nama Barang</label>
       <input type="text" name="nama_barang" class="form-control" required>
     </div>
+
     <div class="mb-3">
       <label class="form-label">Harga Jual</label>
       <input type="number" name="harga_jual" class="form-control" required>
     </div>
     <div class="mb-3">
-      <label class="form-label">Satuan Barang</label>
-      <input type="text" name="satuan_barang" class="form-control" required>
+      <label class="form-label">Jumlah Stok</label>
+      <input type="number" name="stok" class="form-control" min="0" required>
     </div>
     <button type="submit" class="btn btn-success">Simpan</button>
     <a href="barang.php" class="btn btn-secondary">Kembali</a>
